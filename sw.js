85b2e577a7dc2c8cfba7fa237222082b38127860
PRAKASH SWAMI <prakashswamiconstruction@gmail.com>
2026-08-23 08:33:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,41 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
 });
+const CACHE_NAME = 'swami-constructions-v1';
+const ASSETS_TO_CACHE = [
+  '/prakash-swami-constructions/',
+  '/prakash-swami-constructions/index.html',
+  '/prakash-swami-constructions/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
